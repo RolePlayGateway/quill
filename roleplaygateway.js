09ff246b98dc4a25cfaf78f1c87894d1a910185e
Eric Martindale <eric@ericmartindale.com>
2016-03-13 19:51:30 -0700

    Make dates required fields, add client-side support.

--- roleplaygateway.js
+++ roleplaygateway.js
@@ -1,8 +1,3 @@
 var roleplaygateway = require('./lib/roleplaygateway');
 
-roleplaygateway.start(function(err) {
-  roleplaygateway.app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  });
-});
+roleplaygateway.start();
