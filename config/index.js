2ba200aeb050373508f88e0c8fee79840824a6c0
Eric Martindale <eric@ericmartindale.com>
2016-08-05 23:22:38 -0700

    Upgrade to @martindale/maki#community branch.

--- config/index.js
+++ config/index.js
@@ -1,10 +1,20 @@
 module.exports = {
   service: {
-    name: 'RolePlayGateway', // change this!
+    name: 'Quill', // change this!
     synopsis: 'Write better, together.', // and this...
     mission: 'Create your own world with RolePlayGateway\'s collaborative writing tools.', // also
     description: 'RolePlayGateway helps you collaborate with fellow writers to create your own world. We\'ve created a set of roleplaying tools to help you organize your worlds and write with your fellow authors.',
-    icon: 'pencil' // pick from: http://semantic-ui.com/elements/icon.html
+    icon: 'pencil', // pick from: http://semantic-ui.com/elements/icon.html
+    points: [
+      {
+        header: 'e',
+        description: 'Maki can build web, desktop, <em>and</em> native mobile apps, directly from your definitions.  With consistent interactions across them all.',
+        action: {
+          text: 'Read the Docs &raquo;',
+          link: '/docs'
+        }
+      },
+    ]
   },
   database: {
     name: 'roleplaygateway' // only change if you know what you're doing
